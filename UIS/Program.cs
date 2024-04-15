42836ff708f381795c881eec16b5418c074c828a
stoces <stoces@STOCES-PC-01>
2024-04-15 13:42:02 +0200

    pridan komentar

--- UIS/Program.cs
+++ UIS/Program.cs
@@ -12,6 +12,7 @@
 
         static int NactiCislo(string text = "Zadejte cislo: ", int pocetPokusu = 3, int altHodnota = 0)
         {
+            //pridan komentar
             bool povedlose = true;
             int cislo;
             do
